9f7a4b9b26ba15ead7d68ef91f4793fc1752b75f
xupeng <xupeng9527@outlook.com>
2020-06-29 02:03:42 +0800

    代码提交

--- microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
@@ -1,8 +1,10 @@
 package com.atguigu.springcloud.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.atguigu.springcloud.entities.Dept;
 import com.atguigu.springcloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,6 +14,9 @@ public class DeptController {
 
     @Autowired
     private DeptService service;
+
+    @Autowired
+    private DiscoveryClient client;
 
     @RequestMapping(value = "/dept/add", method = RequestMethod.POST)
     public boolean add(@RequestBody Dept dept){
@@ -27,5 +32,11 @@ public class DeptController {
     public List<Dept> list(){
         return service.list();
     }
+
+    @RequestMapping(value = "/dept/serviceList", method = RequestMethod.GET)
+    public String serviceList(){
+        return JSON.toJSONString(client.getServices());
+    }
+
 
 }
