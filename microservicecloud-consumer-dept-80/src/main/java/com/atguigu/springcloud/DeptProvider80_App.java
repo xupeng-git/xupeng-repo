85ba6db52eccecd06a934383e20f1f9ee386cab3
xupeng <xupeng@sinosoft.com.cn>
2020-06-28 17:51:16 +0800

    微服务项目提交

---
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/DeptProvider80_App.java
@@ -0,0 +1,12 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeptProvider80_App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeptProvider80_App.class, args);
+    }
+}
